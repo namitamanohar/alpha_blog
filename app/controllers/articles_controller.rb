c2ff5811b3817aa356fbabd2d0b2d7ca5b291639
namitamanohar <manohar.namita@gmail.com>
2021-10-08 09:58:58 -0400

    added edit feature to article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,15 +12,32 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end  
 
+  def edit 
+    #need to initiate the article instance 
+    @article = Article.find(params[:id])
+  end 
+
   def create 
     # render plain: params[:article]
     @article = Article.new(params.require(:article).permit(:title, :description))
     if @article.save
       flash[:notice] = "Article has been saved."
-    #redirect_to article_path(@article)
+      #redirect_to article_path(@article)
       redirect_to @article
     else 
       render 'new'
     end 
   end 
+  
+  def update 
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description)) 
+      flash[:notice] = "Article was updated "
+      redirect_to @article
+    else
+      render 'edit'
+    end 
+  end 
+  
+
 end
