dcc1af195e01225f7f6b0fd817bc448aa30942e7
namitamanohar <manohar.namita@gmail.com>
2021-10-11 10:37:45 -0400

    Extracted redundancies and refactoring

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController 
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
   def show 
-    @article = Article.find(params[:id])
+
   end 
 
   def index 
@@ -13,13 +14,13 @@ class ArticlesController < ApplicationController
   end  
 
   def edit 
-    #need to initiate the article instance 
-    @article = Article.find(params[:id])
+    #need to initiate the article instance now done using set_article
   end 
 
   def create 
     # render plain: params[:article]
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    #@article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article has been saved."
       #redirect_to article_path(@article)
@@ -30,8 +31,8 @@ class ArticlesController < ApplicationController
   end 
   
   def update 
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description)) 
+    #if @article.update(params.require(:article).permit(:title, :description)) 
+    if @article.update(article_params) 
       flash[:notice] = "Article was updated "
       redirect_to @article
     else
@@ -39,5 +40,17 @@ class ArticlesController < ApplicationController
     end 
   end 
   
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
 
+  private 
+  def set_article 
+    @article = Article.find(params[:id])
+  end
+
+  def article_params 
+   params.require(:article).permit(:title, :description)
+  end
 end
