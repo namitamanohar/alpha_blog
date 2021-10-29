0015cd315e26fe1c806fb36bd1685d7a0f43ddee
namitamanohar <manohar.namita@gmail.com>
2021-10-29 13:48:25 -0400

    added admin, destroy user, current user functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+  #makes current_user available to the views 
+  helper_method :current_user, :logged_in?
+  def current_user 
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end 
+
+  def logged_in?
+    #turn current_user into a boolean using !!
+    !!current_user
+  end 
+
+  def require_user 
+    if !logged_in? 
+      flash[:alert] = "You must be logged in to perform that action"
+      redirect_to login_path
+    end
+  end 
+
 end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,8 @@
 class ArticlesController < ApplicationController 
   before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, except: [:show, :index]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+
   def show 
 
   end 
@@ -21,7 +24,7 @@ class ArticlesController < ApplicationController
     # render plain: params[:article]
     #@article = Article.new(params.require(:article).permit(:title, :description))
     @article = Article.new(article_params)
-    @article.user = User.first 
+    @article.user = current_user
     if @article.save
       flash[:notice] = "Article has been saved."
       #redirect_to article_path(@article)
@@ -53,5 +56,12 @@ class ArticlesController < ApplicationController
 
   def article_params 
    params.require(:article).permit(:title, :description)
+  end
+
+  def require_same_user 
+    if current_user != @article.user && !current_user.admin?
+      flash[:alert] = "You can only edit or delete your own article"
+      redirect_to @article 
+    end
   end
 end
